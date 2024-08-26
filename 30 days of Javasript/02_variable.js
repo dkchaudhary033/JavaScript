ea8311bb5d5ae9aff17613a61e0febe062ab0e11
Deepak Chaudhary <99008135+dkchaudhary033@users.noreply.github.com>
2024-08-26 04:05:44 +0000

    variable

---
+++ 30 days of Javasript/02_variable.js
@@ -0,0 +1,9 @@
+const accountId = 56016;
+let accountName = 'Deepak';
+var accountType = 'Saving';
+cityName = 'Agra';
+
+console.table([accountId, accountName, accountType, cityName]);
+
+// we are not use a var variable beacause of its scope limitation
+
---
+++ 30 days of Javasript/03_datatype.js
@@ -0,0 +1,4 @@
+"use strict"; // treate all JS code as a newer Javascript version
+
+// alert (3+3) //we are using nodejs , not a browser.
+
